c880acf41d26ae6c22bdab0e8de020b4d6a8c63c
Zsolt Simon <zsolt.simon@labcup.net>
2023-08-05 17:09:11 +0200

    added prisma

--- index.js
+++ index.js
@@ -1,93 +1,111 @@
-const express = require('express');
+const express = require("express");
 
 // BE app:
 const app = express();
-require('dotenv/config');
+require("dotenv/config");
 const PORT = process.env.PORT || 8000;
-const { Pool } = require('pg');
-const cors = require('cors');
+const { Pool } = require("pg");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
 // Database:
 const pool = new Pool({
   connectionString: process.env.ELEPHANT_SQL_CONECTION_STRING,
 });
 
+const prisma = new PrismaClient();
+
 app.use(cors());
 app.use(express.json());
 
 //CRUD
 // Get all movies:
-app.get('/api/movies', (req, res) => {
-  pool
-    .query('SELECT * FROM movies;')
-    .then((data) => {
-      console.log(data);
-      res.json(data.rows);
-    })
-    .catch((e) => res.status(500).json({ message: e.message }));
+app.get("/api/movies", async (req, res) => {
+  const movies = await prisma.movies.findMany();
+
+  if (!movies) {
+    res.status(404).json({ message: "No movies found" });
+  }
+
+  res.json(movies);
 });
 
 //Get 3 Random movies:
-app.get('/api/movies/random', (req, res) => {
-  pool
-    .query('SELECT * FROM movies ORDER BY RANDOM() LIMIT 1;')
+app.get("/api/movies/random", async (req, res) => {
+  const ids =
+    await prisma.$queryRaw`SELECT id FROM movies ORDER BY RAND() LIMIT ${3}`;
+
+  await prisma.movies
+    .findMany({
+      where: {
+        ids,
+      },
+      orderBy: {
+        id: "asc",
+      },
+    })
     .then((data) => {
       console.log(data);
       res.json(data.rows);
     })
     .catch((e) => res.status(500).json({ message: e.message }));
+
+  // pool
+  //   .query('SELECT * FROM movies ORDER BY RANDOM() LIMIT 1;')
+  //   .then((data) => {
+  //     console.log(data);
+  //     res.json(data.rows);
+  //   })
+  //   .catch((e) => res.status(500).json({ message: e.message }));
 });
 
 app.listen(PORT, () => console.log(`SERVER IS UP ON ${PORT}`));
 
 // Get movie by id:
-app.get('/api/movies/:id', (req, res) => {
+app.get("/api/movies/:id", (req, res) => {
   const { id } = req.params;
-  const safeValues = [id]; //this need to be array!!
-  pool
-    .query('SELECT * FROM movies WHERE id=$1;', safeValues)
-    .then(({ rowCount, rows }) => {
-      if (rowCount === 0) {
+  prisma.movies
+    .findFirst({
+      where: {
+        id: Number(id),
+      },
+    })
+    .then((movie) => {
+      if (!movie) {
         res.status(404).json({ message: `Movie with id ${id} Not Found` });
       } else {
-        console.log(rows);
-        res.json(rows[0]);
+        res.json(movie);
       }
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
 
 // Add movie:
-app.post('/api/movies', (req, res) => {
+app.post("/api/movies", (req, res) => {
   const { title, director, year, genre, rating, poster, plot, duration } =
     req.body;
-  console.log(req.body);
-
-  const safeValues = [
-    title,
-    director,
-    year,
-    genre,
-    poster,
-    rating,
-    plot,
-    duration,
-  ];
-
-  pool
-    .query(
-      'INSERT INTO movies (title,director,year,genre,poster,rating,plot,duration) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;',
-      safeValues
-    )
-    .then(({ rows }) => {
-      console.log(rows);
-      res.status(201).json(rows[0]);
+  
+  prisma.movies
+    .create({
+      data: {
+        title,
+        director,
+        year,
+        genre,
+        rating,
+        poster,
+        plot,
+        duration,
+      },
+    })
+    .then((movie) => {
+      res.status(201).json(movie);
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
 
 // Update movie:
-app.put('/api/movies/:id', (req, res) => {
+app.put("/api/movies/:id", (req, res) => {
   const { id } = req.params;
   const { title, director, year, genre, rating, poster, plot, duration } =
     req.body;
@@ -104,7 +122,7 @@ app.put('/api/movies/:id', (req, res) => {
   ];
   pool
     .query(
-      'UPDATE movies SET title=$1,director=$2,year=$3,genre=$4,rating=$5,poster=$6,plot=$7,duration=$8 WHERE id=$9 RETURNING *;',
+      "UPDATE movies SET title=$1,director=$2,year=$3,genre=$4,rating=$5,poster=$6,plot=$7,duration=$8 WHERE id=$9 RETURNING *;",
       safeValues
     )
     .then(({ rows }) => {
@@ -115,11 +133,11 @@ app.put('/api/movies/:id', (req, res) => {
 });
 
 // Delete movie:
-app.delete('/api/movies/:id', (req, res) => {
+app.delete("/api/movies/:id", (req, res) => {
   const { id } = req.params;
   const safeValues = [id];
   pool
-    .query('DELETE FROM movies WHERE id=$1 RETURNING *;', safeValues)
+    .query("DELETE FROM movies WHERE id=$1 RETURNING *;", safeValues)
     .then(({ rows }) => {
       console.log(rows);
       res.json(rows[0]);
